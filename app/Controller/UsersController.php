7f6b57307264a97f9f1ff00d6a7090d779eef5e9
zipate <eddieokemwa@gmail.com>
2019-08-16 12:48:19 +0300

    mbele yako

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -626,8 +626,8 @@ class UsersController extends AppController {
         $this->Acl->allow($group, 'controllers/Notifications/delete');
         $this->Acl->allow($group, 'controllers/Organizations/delete');
         $this->Acl->allow($group, 'controllers/PreviousDates/delete');
-        $this->Acl->allow($group, 'controllers/StudyRoute/delete');
-        $this->Acl->allow($group, 'controllers/Manufacturer/delete');
+        $this->Acl->allow($group, 'controllers/StudyRoutes/delete');
+        $this->Acl->allow($group, 'controllers/Manufacturers/delete');
         $this->Acl->allow($group, 'controllers/Placebos/delete');
         $this->Acl->allow($group, 'controllers/SiteDetails/delete');
         $this->Acl->allow($group, 'controllers/Sponsors/delete');
